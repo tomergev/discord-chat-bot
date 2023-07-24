957919b8103f57ab80804992afee0cab2760788f
Tomer <tomergev21@gmail.com>
2023-07-24 14:19:16 -0700

    Started working on dynmically fetching all guildIds. Currently fetching all guild ids when client is ready. Need to work on fetching all guildIds when a new server is added

--- index.js
+++ index.js
@@ -11,61 +11,62 @@ const {
 } = require('discord.js')
 const get = require('lodash.get')
 const { generateResponse } = require('./openai')
-const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits
 
-try {
-  const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] })
-  
-  client.login(process.env.DISCORD_TOKEN_SECRET)
-  client.once(Events.ClientReady, (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`)
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const rest = new REST().setToken(process.env.DISCORD_TOKEN_SECRET)
+
+const chatGptCommand = {
+  data: new SlashCommandBuilder()
+    .setName('ask')
+    .setDescription('What would you like to know?')
+    .addStringOption((option) => (
+      option.setName('prompt')
+        .setDescription('What would you like to know?')
+        .setRequired(true)
+    )),
+    async execute(interaction) {
+      await interaction.deferReply({ ephemeral: true })
+      const prompt = interaction.options.getString('prompt')
+      const { data } = await generateResponse(`${prompt}. The response must be less than 2000 characters`) || {}
+      const reply = get(data, 'choices.0.message.content')
+      interaction.editReply(reply)
+    },
+}
+
+client.login(process.env.DISCORD_TOKEN_SECRET)
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+  const guilds = c?.guilds?.cache || []
+  guilds.forEach((guild) => {
+    rest.put(
+      Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, guild.id),
+      { body: [chatGptCommand.data] },
+    )
   })
-  
-  const chatGptCommand = {
-    data: new SlashCommandBuilder()
-      .setName('ask')
-      .setDescription('What would you like to know?')
-      .addStringOption((option) => (
-        option.setName('prompt')
-          .setDescription('What would you like to ask me?')
-          .setRequired(true)
-      )),
-      async execute(interaction) {
-        try {
-          await interaction.deferReply({ ephemeral: true })
-          const prompt = interaction.options.getString('prompt')
-          const { data } = await generateResponse(`${prompt}. The response must be less than 2000 characters`) || {}
-          const reply = get(data, 'choices.0.message.content')
-          interaction.editReply(reply)
-        } catch (err) {
-          // console.error(err)
-        }
-      },
+})
+
+client.commands = new Collection()
+client.commands.set(chatGptCommand.data.name, chatGptCommand)
+
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
+
+  const command = interaction?.client?.commands?.get(interaction.commandName)
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
   }
 
-  client.commands = new Collection()
-  client.commands.set(chatGptCommand.data.name, chatGptCommand)
-  
-  const rest = new REST().setToken(process.env.DISCORD_TOKEN_SECRET)
-  const guildId = '1131699017898803391'
-  rest.put(
-    Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, guildId),
-    { body: [chatGptCommand.data] },
-  )
-
-  client.on(Events.InteractionCreate, (interaction) => {
-    if (!interaction.isChatInputCommand()) return
-  
-    const command = interaction.client.commands.get(interaction.commandName)
-  
-    if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`)
-      return
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
     }
-  
-    command.execute(interaction)
-  })
-
-} catch (err) {
-  // console.error(err)
-}
+  }
+})
