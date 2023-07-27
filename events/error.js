37e8e00a77dde32d2bdc1c7a17c4aaf11b1aca4b
Tomer <tomergev21@gmail.com>
2023-07-27 15:50:29 -0700

    Creating a port for render

--- events/error.js
+++ events/error.js
@@ -2,7 +2,6 @@ const { Events } = require('discord.js')
 
 module.exports = (client) => {
   client.on(Events.Error, (err) => {
-    console.log('events/error')
     console.error(err)
   })
 }
--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const {
   REST, 
   Routes, 
 } = require('discord.js')
+const express = require('express')
 const askCommand = require('./commands/ask')
 const clientReadyEvent = require('./events/clientReady')
 const errorEvent = require('./events/error')
 const guildCreateEvent = require('./events/guildCreate')
 const interactionCreateEvent = require('./events/interactionCreate')
+
+// This code is only here to fix an issue with deploying on render: https://render.com/docs/web-services#host-and-port-configuration
+const app = express()
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
+})
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 const commands = [askCommand.data]
